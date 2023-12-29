da6d44f007fea163da3c8c9d324f2963a7712957
Paweł Machnik <machnik.pawel@gmail.com>
2023-12-29 12:11:12 +0100

    Increase maintainability by definity new function containing existing logic

--- stats.py
+++ stats.py
@@ -2,14 +2,17 @@ import json
 from pprint import pprint
 from operator import itemgetter
 
-filename = 'data/get-users-top-artists-and-tracks-medium_term.json'
-# filename = 'data/get-users-top-artists-and-tracks-long_term.json'
 
-with open(filename) as fp:
-    data = json.load(fp)
-    # print(json.dumps(data, indent=1, sort_keys=True))
-    # print(list(data.keys()))
-    # pprint(data['items'])
-    sorted_list = sorted(data['items'], key=itemgetter('popularity')) 
-    formatted_list = list(map(lambda item: f"{item['popularity']} {item['artists'][0]['name']} - {item['name']}",sorted_list))
-    pprint(formatted_list)
+def print_users_top_tracks(time_range='medium_term'):
+    """Print sorted list of most popular tracks played by user.
+
+    Keyword arguments:
+    time_range -- string that can get 2 values: medium_term or long_term
+    """
+    with open(f'data/get-users-top-artists-and-tracks-{time_range}.json') as fp:
+        data = json.load(fp)
+        sorted_list = sorted(data['items'], key=itemgetter('popularity')) 
+        formatted_list = list(map(lambda item: f"{item['popularity']} {item['artists'][0]['name']} - {item['name']}",sorted_list))
+        pprint(formatted_list)
+
+print_users_top_tracks('long_term')
